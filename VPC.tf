8c3ab33885c5df1bcc11fa3c0006e3e92d5af984
Varisha <varisha.awsdevmule@gmail.com>
2021-07-05 01:34:49 +0530

    TF - multiple instance creation using for_each loop

--- VPC.tf
+++ VPC.tf
@@ -1,41 +1,41 @@
-resource "aws_vpc" "TF_VPC"{
-    cidr_block = "${var.vpc_cidr}"
-    tags = {
-        Name = "${var.vpc_name}"
-    }
+resource "aws_vpc" "TF_VPC" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = var.vpc_name
+  }
 }
 
 resource "aws_internet_gateway" "testGW" {
-    vpc_id = aws_vpc.TF_VPC.id
-    tags = {
-        Name = "${var.IGW_name}"
-    }
+  vpc_id = aws_vpc.TF_VPC.id
+  tags = {
+    Name = var.IGW_name
+  }
 }
 
 resource "aws_subnet" "subnets" {
-    count = 3
-    vpc_id = aws_vpc.TF_VPC.id
-    map_public_ip_on_launch = true
-    cidr_block = "${element(var.blocks,count.index)}"
-    availability_zone = "${element(var.azs,count.index)}"
-    tags = {
-        Name = "TF_subnet_${count.index+1}"
-    }
+  count                   = 3
+  vpc_id                  = aws_vpc.TF_VPC.id
+  map_public_ip_on_launch = true
+  cidr_block              = element(var.blocks, count.index)
+  availability_zone       = element(var.azs, count.index)
+  tags = {
+    Name = "TF_subnet_${count.index + 1}"
+  }
 }
 
 resource "aws_route_table" "route-public" {
-    vpc_id = aws_vpc.TF_VPC.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.testGW.id
-    }
-    tags = {
-        Name = "${var.Main_Routing_Table}"
-    }
+  vpc_id = aws_vpc.TF_VPC.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.testGW.id
+  }
+  tags = {
+    Name = var.Main_Routing_Table
+  }
 }
 
 resource "aws_route_table_association" "TF_route" {
-    count = "${length(var.blocks)}"
-    subnet_id = "${element(aws_subnet.subnets.*.id,count.index)}"
-    route_table_id = aws_route_table.route-public.id
+  count          = length(var.blocks)
+  subnet_id      = element(aws_subnet.subnets.*.id, count.index)
+  route_table_id = aws_route_table.route-public.id
 }
--- instance.tf
+++ instance.tf
@@ -1,25 +1,22 @@
-/*
 data "aws_subnet_ids" "my_subnet_ids" {
-    vpc_id = "${aws_vpc.TF_VPC.id}"
+  vpc_id = aws_vpc.TF_VPC.id
 }
-
-data "aws_subnet" "subnets" {
-    count = "${length(data.aws_subnet_ids.my_subnet_ids.ids)}"
-    id    = "${data.aws_subnet_ids.my_subnet_ids.ids[count.index]}"
-}
-
-resource "aws_instance" "EC2_with_TF" {
-    ami = "${lookup(var.amis,var.aws_region)}"
-    instance_type = "t2.micro"
-    key_name = "${var.key_name}"
-    availability_zone = "us-east-1"
-    vpc = aws_vpc.TF_VPC.id
-    subnet_id = "${element(data.aws_subnet_ids.my_subnet_ids.ids, count.index)}"
-    vpc_security_group_ids=["${aws_security_group.TF_SG.id}"]
-    associate_public_ip_address = true
-
-    tags = {
-        Name = "AnsiSlave${count.index+1}"
-    }
+/*
+data "aws_subnet_ids" "subnets" {
+  count  = length(data.aws_subnet_ids.my_subnet_ids.id)
+  id     = data.aws_subnet_ids.my_subnet_ids.id[count.index]
 }
 */
+resource "aws_instance" "EC2_with_TF" {
+  for_each                    = data.aws_subnet_ids.my_subnet_ids.ids
+  ami                         = lookup(var.amis, var.aws_region)
+  instance_type               = "t2.micro"
+  key_name                    = var.key_name
+  subnet_id                   = each.value
+  vpc_security_group_ids      = [aws_security_group.TF_SG.id]
+  associate_public_ip_address = true
+  
+    tags = {
+    Name = "AnsiSlave-${each.value}"
+  }
+}
